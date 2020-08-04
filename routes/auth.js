736c22a2fcef636bf0c9b46ecc9d5d3399fd122e
Reggio <contact@graphandco.com>
2020-08-04 12:58:24 +0200

    Add Joi validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+//VALIDATION
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+});
+
 router.post('/register', async (req, res) => {
+    //VALIDATE DATA WITH JOY BEFORE SUBMIT USE
+    const { error } = schema.validate(req.body);
+    //res.send(error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
