357cf77eddce6f97c120e0d8cb964f9ac6d418c3
Reggio <contact@graphandco.com>
2020-08-04 13:18:28 +0200

    Check if email already exists

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,13 @@ const User = require('../model/User');
 const { registerValidation, loginValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
-    // //VALIDATE DATA WITH JOY BEFORE SUBMIT USE
-    // const { error } = schema.validate(req.body);
-    // //res.send(error.details[0].message);
+    //VALIDATE DATA WITH JOY BEFORE SUBMIT USE
     const { error } = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
+
+    //CHECK IF USER ALREADY EXISTS IN DB
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send('Cet email est déjà utilisé');
 
     const user = new User({
         name: req.body.name,
