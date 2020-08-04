5709f62aeac874b650042278cacccea46f6968b6
Reggio <contact@graphandco.com>
2020-08-04 12:05:59 +0200

    Connect to DB with .env

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log('Connected to MongoDB');
+    }
+);
 
 //Import routes
 const authRoute = require('./routes/auth');
